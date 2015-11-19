12e6437b67501e84172a932884e0b6ba9f10b8e5
Ivaylo Ivanov <ivchoyyyy@gmail.com>
2015-11-19 22:20:20 +0200

    init commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Part of esrol decoupling procedure.
---
+++ tests/test.js
@@ -0,0 +1,13 @@
+'use strict';
+let expect = require('chai').expect;
+let Servers = require('../index.js');
+let mocks = require ('./mocks/index');
+require('mocha-sinon');
+
+describe('Servers Success...', () => {
+
+});
+
+describe('Servers Fail...', () => {
+     
+});
